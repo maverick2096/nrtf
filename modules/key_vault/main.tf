5dc1848f6c8c8a7fd1cbd405a09e462caca38180
MaverickTG2096 <abhishek@brillianttechniques.com>
2025-10-01 03:12:54 +0530

    commit 9f1c3e2d5b6a7e8f9a0b1c2d3e4f5g6h7i8j9k0l

--- modules/key_vault/main.tf
+++ modules/key_vault/main.tf
@@ -12,5 +12,19 @@ resource "azurerm_key_vault" "this" {
   tags                          = var.tags
 }
 
+resource "azurerm_private_endpoint" "kv"{
+  name                = "${var.name}-pe"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = azurerm_subnet.pep.id
+  tags                = var.tags
+
+  private_service_connection {
+    name                           = "${var.name}-psc"
+    private_connection_resource_id = azurerm_key_vault.kv.id
+    is_manual_connection           = false
+    subresource_names              = ["vault"]
+  }
+}
 output "id" { value = azurerm_key_vault.this.id }
 output "vault_uri" { value = azurerm_key_vault.this.vault_uri }
